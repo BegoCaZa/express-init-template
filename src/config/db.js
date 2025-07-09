8fc52df235e07e4691b109d1f7bdfdcd3f8cca24
BegoCaZa <begoxcz@hotmail.com>
2025-07-09 12:33:14 +0200

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL)
+console.log("Conectado a BD");
+mongoose.connect(process.env.MONGO_URL);
